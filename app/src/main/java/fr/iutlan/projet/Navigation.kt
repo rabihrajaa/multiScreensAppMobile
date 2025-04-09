6762fd850a31c44386cc7f75597909362b30b3f1
rabihrajaa <rajaarabihh@gmail.com>
2025-04-09 21:57:59 +0100

    Initial commit

---
+++ app/src/main/java/fr/iutlan/projet/Navigation.kt
@@ -0,0 +1,19 @@
+// ui/components/NavigationButton.kt
+package ui.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun NavigationButton(
+    navController: NavController,
+    label: String,
+    destination: String
+) {
+    Button(onClick = { navController.navigate(destination) }) {
+        Text(text = label)
+    }
+}
